41d12edd7029c91dc0f148fde03f57dbe137426f
ELBEKJON <elbektogayev92@gmail.com>
2023-12-03 20:45:52 +0500

    Swiper slider file addd .....

--- js/main.js
+++ js/main.js
@@ -1,62 +1,31 @@
 var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 3,
-    spaceBetween: 16,
-    navigation: {
-      nextEl: ".one",
-      prevEl: ".on-e",
+  slidesPerView: 3,
+  spaceBetween: 30,
+  navigation: {
+    nextEl: ".one",
+    prevEl: ".button-",
+  },
+  loop: true,
+  breakpoints: {
+    992: {
+      slidesPerView: 3,
+      spaceBetween: 30,
     },
-    loop: true,
-    breakpoints: {
-      992: {
-        slidesPerView: 3,
-        spaceBetween: 30,
-      },
-        760: {
-        slidesPerView: 2.5,
-        spaceBetween: 20,
-      },
-        630: {
-        slidesPerView: 2,
-        spaceBetween: 30,
-      },
-        480: {
-        slidesPerView: 1.5,
-        spaceBetween: 20,
-      },
-        320: {
-        slidesPerView: 1,
-        spaceBetween: 20,
-      },
-    }
-  });
-  var swiper = new Swiper(".mySwiper-one", {
-    slidesPerView: 3,
-    spaceBetween: 16,
-    navigation: {
-      nextEl: ".next-two",
-      prevEl: ".prev-tw-o",
+      760: {
+      slidesPerView: 2.5,
+      spaceBetween: 20,
     },
-    loop: true,
-    breakpoints: {
-      992: {
-        slidesPerView: 3,
-        spaceBetween: 30,
-      },
-        760: {
-        slidesPerView: 2.5,
-        spaceBetween: 20,
-      },
-        630: {
-        slidesPerView: 2,
-        spaceBetween: 30,
-      },
-        480: {
-        slidesPerView: 1.5,
-        spaceBetween: 20,
-      },
+      630: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+      480: {
+      slidesPerView: 1.5,
+      spaceBetween: 20,
+    },
       320: {
-        slidesPerView: 1,
-        spaceBetween: 20,  
-      }
-    }
-  });
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+  }
+});
